9bc684c22d46452433dab0e937eff1442a9c7db8
Елена Фрыкина <elenafrykina@MacBook-Pro-Elena.local>
2019-05-25 01:32:28 +0300

    change form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+//modals
 
     let popupEngineer = document.querySelector('.popup_engineer_btn'), 
         phoneLink = document.querySelector('.phone_link'),
@@ -38,13 +39,71 @@
 
         });
 
-        
-        
-      
+// forms
 
+  let form = document.querySelector('.main_form');
+//   console.log('main_form');
+
+function sendForm(elem) {
+    let input = document.getElementsByTagName('.form_input'),
+    statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+
+    let message = {
+        loading: 'Идет отправка',
+        success: 'Отправлено!',
+        failure: 'Ошибка...'
+    };
+
+    elem.addEventListener('submit', function(e) {
+        e.preventDefault();
+        elem.appendChild(statusMessage);
+        let formData = new FormData(elem);
+
+        function postData() {
+            return new Promise(function(resole,reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState < 4 ) {
+                        resolve();
+                    } else if (request.readyState === 4 && request.status == 200) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                });
+
+    request.send(formData);
+
+            });
+        }
+        function clearInput() {
+            for(let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
     
+			postData(formData)
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(() => statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput);
+
+    });
+}
+
+sendForm(form);
+
+let inputPhone = document.querySelectorAll('input[type="tel"]');
+for(let i = 0; i < inputPhone.length; i++){
+    inputPhone[i].addEventListener('inout', function() {
+    inputPhone[i].value = inputPhone[i].value.replace(/[^+\d]/g, '');
+    });
+}
 
 
-
-
+// В ПРОЦЕССЕ, запушила для себя пока
 
